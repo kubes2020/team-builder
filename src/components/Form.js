dcc790a1e776209e92d59d15a151a1d9d92671ab
Brian Kubes <briankubes@gmail.com>
2020-09-15 08:57:55 -0700

    still going

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 
-const TeamForm = () => {
+export default function Form() {
   const [friends, setFriends] = useState({
     fname: "",
   });
 
   const handleChange = (e) => {
     setFriends({ fname: e.target.value });
+    console.log(friends);
   };
 
   return (
-    <form>
-      <label htmlFor="fname">
-        First Name:
-        <input onChange={handleChange} id="fname" type="text" />
-      </label>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="fname">First Name:</label>
+      <input onChange={handleChange} id="fname" type="text" />
+      <label htmlFor="lname">Last Name:</label>
+      <input onChange={handleChange} id="lname" type="text" />
+      <button type="submit">Submit!</button>
     </form>
   );
-};
+}
